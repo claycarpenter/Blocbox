96c1e09f194e1c04fc941281257345171c6fcc4f
Clay Carpenter <claycarpenter@gmail.com>
2015-06-18 17:06:14 -0400

    Adds account settings pop over menu.

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
     // Add Item dropdown menu.
     $('.menu').dropit();
 
+    // Account settings dropdown menu.
+    $('.pop-north').dropit();
+
     // Small-screen nav menu.
     $('#smallNav').click(function () {
         console.log('up');
