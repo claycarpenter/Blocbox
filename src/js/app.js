93b285a26c6b2697e87f5558bc66733b4decd4f2
Clay Carpenter <claycarpenter@gmail.com>
2015-06-18 16:01:32 -0400

    Adds responsive navigation menu.

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
+    // Add Item dropdown menu.
     $('.menu').dropit();
+
+    // Small-screen nav menu.
+    $('#smallNav').click(function () {
+        console.log('up');
+        $('#left-nav').toggleClass('open', 200);
+    });
 });
